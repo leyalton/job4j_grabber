cfae68022b00dd2a0df2283b888ded0cca286deb
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-25 00:17:57 +0300

    Запуск Travis CI.

--- src/main/java/ru/job4j/quartz/Main.java
+++ src/main/java/ru/job4j/quartz/Main.java
@@ -1,4 +1,14 @@
 package ru.job4j.quartz;
 
 public class Main {
+    public int getVal() {
+        return val;
+    }
+
+    public final int val;
+
+    public Main(int val) {
+        this.val = val;
+    }
+
     public static void main(String [] args) {System.out.println("Hello world");}}
