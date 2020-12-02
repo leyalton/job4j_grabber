14ff8aa9b85c987ce7903a42bf09d3ee118f781e
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-12-02 21:54:26 +0300

    1.1. Job c параметрами[260360#334385]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,66 +3,61 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.*;
-import java.sql.*;
-import java.util.*;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
+    private Properties properties;
+    Connection connection = null;
+
+    // Чтение из конфигурационного файла
+    // Properties properties = new Properties();
     public void start() {
-        try {
-            // 1. Конфигурирование.
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-
-            JobDataMap data = new JobDataMap();
-            data.put("connection", this.getConnection());
-
-            // 2. Создание задачи.
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
-
-            // 3. Создание расписания.
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(5)
-                    .repeatForever();
-
-            // 4. Задача выполняется через триггер.
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-
-            // 5. Загрузка задачи и триггера в планировщик
-            scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
-        } catch (Exception se) {
-            se.printStackTrace();
-        }
-    }
-
-    public Connection getConnection() {
-        Connection connection = null;
-        // считывания данных из конфигурационного файла
         try (InputStream inputStream = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            Properties properties = new Properties();
-            // загрузить данные, содержащие в обьекте класса Properties
             properties.load(inputStream);
-
+            // подключение к бд
             Class.forName(properties.getProperty("driver"));
             connection = DriverManager.getConnection(
                     properties.getProperty("url"),
                     properties.getProperty("username"),
-                    properties.getProperty("password"));
-        } catch (IOException | ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+                    properties.getProperty("password")); {
+                JobDataMap data = new JobDataMap();
+                data.put("connection", connection);
+
+                // 1. Конфигурирование.
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+
+                // 2. Создание задачи.
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+
+                // 3. Создание расписания.
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(properties.getProperty("rabbit.interval")))
+                        .repeatForever();
+
+                // 4. Задача выполняется через триггер.
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+
+                // 5. Загрузка задачи и триггера в планировщик
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
+        } catch (Exception se) {
+            se.printStackTrace();
         }
-        return connection;
     }
 
     public static void main(String[] args) {
