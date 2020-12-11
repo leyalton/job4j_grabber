183d22ecb3737c6683d598f008fa3806351745f2
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-12-12 01:05:21 +0300

    codecov

---
+++ src/main/java/ru/job4j/HelloWorld.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class HelloWorld {
+
+    public String getMessage(boolean bigger) {
+        if (bigger) {
+            return "Hello Universe!";
+        } else {
+            return "Hello World!";
+        }
+    }
+}
