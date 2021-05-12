167788e3bb231a6d2df81d683b2f0fc88644c463
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2021-05-12 17:33:32 +0300

    1. Схема прав пользователи и роли.[1093#334397]

--- src/main/java/ru/job4j/sql/create.sql
+++ src/main/java/ru/job4j/sql/create.sql
@@ -2,62 +2,62 @@
 
 create table rule
 (
-    id   serial primary key,
-    name varchar(255) not null
+    rule_id   serial primary key,
+    rule_name varchar(255) not null
 );
 
 create table role
 (
-    id   serial primary key,
-    name varchar(255) not null
+    role_id   serial primary key,
+    rule_name varchar(255) not null
 );
 
 create table role_rule
 (
-    id      serial primary key,
-    rule_id int references rule (id),
-    role_id int references role (id)
+    role_rule_id serial primary key,
+    rule         int references rule (rule_id),
+    role         int references role (role_id)
 );
 
 create table users
 (
-    id   serial primary key,
-    name varchar(255),
-    role int references role (id)
+    users_id   serial primary key,
+    users_name varchar(255),
+    role       int references role (role_id)
 );
 
 create table category
 (
-    id   serial primary key,
-    name varchar(255) not null
+    category_id   serial primary key,
+    category_name varchar(255) not null
 );
 
 create table state
 (
-    id   serial primary key,
-    name varchar(255) not null
+    state_id   serial primary key,
+    state_name varchar(255) not null
 );
 
 create table attach
 (
-    id   serial primary key,
-    name varchar(255) not null
+    attach_id   serial primary key,
+    attach_name varchar(255) not null
 );
 
 create table item
 (
-    id       serial primary key,
-    name     varchar(255) not null,
-    author   int references users (id),
-    category int references category (id),
-    state    int references state (id),
-    file     int references attach (id)
+    item_id   serial primary key,
+    item_name varchar(255) not null,
+    author    int references users (users_id),
+    category  int references category (category_id),
+    state     int references state (state_id),
+    file      int references attach (attach_id)
 );
 
-create table item_comments
+create table comments
 (
-    id serial primary key,
-    comment    varchar(255),
-    author     int references users (id),
-    item       int references item (id)
+    id      serial primary key,
+    comment varchar(255),
+    author  int references users (users_id),
+    item    int references item (item_id)
 );
