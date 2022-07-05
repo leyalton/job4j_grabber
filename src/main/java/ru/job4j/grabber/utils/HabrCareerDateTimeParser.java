1266809c33251987da0ff949d810cead17565a94
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2022-07-06 01:00:30 +0300

    2.1. Преобразование даты

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,10 +1,13 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse);
+        ZonedDateTime time = ZonedDateTime.parse(parse);
+        LocalDateTime date = time.toLocalDateTime();
+        return date;
     }
 }
