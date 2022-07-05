234615996f7de26a16e63fca91403f1c52b6783a
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2022-07-06 00:24:58 +0300

    2.2. Модель данных - Post

--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -5,53 +5,82 @@ import java.util.Objects;
 
 public class Post {
 
-    int id;
-    String title;
-    String link;
-    String description;
-    LocalDateTime created;
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
 
     @Override
     public String toString() {
-        return "Post{"
-                +
-                "id=" + id
-                +
-                ", title='" + title + '\''
-                +
-                ", link='" + link + '\''
-                +
-                ", description='" + description + '\''
-                +
-                ", created=" + created
-                +
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Post)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (!(o instanceof Post)) return false;
         Post post = (Post) o;
-        return id == post.id
-                &&
-                Objects.equals(title, post.title)
-                &&
-                Objects.equals(link, post.link)
-                &&
-                Objects.equals(description, post.description)
-                &&
-                Objects.equals(created, post.created);
-
-
+        return id == post.id &&
+                Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description, created);
+        return Objects.hash(id, link);
     }
 }
